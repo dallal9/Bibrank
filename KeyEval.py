15b4c321159189e4bff702eda8bd323c460a4089
dallal9 <abdelrhman.d@aucegypt.edu>
2023-01-05 03:50:13 +0200

    reformatting

--- KeyEval.py
+++ KeyEval.py
@@ -1,18 +1,22 @@
-import pandas as pd
-from Datasets.utils import read_parquet, get_bib_info, clean
-import json
-import sys
-
-import time
 import csv
+import json
 import random
+import sys
+import time
+from typing import List
+
+import pandas as pd
 from tqdm import tqdm
 
-sys.path.append("Models/")
-from Models import *
-from position_rank import get_weights
+from Datasets.utils import clean, read_parquet
 
-def mask(text1, text2):
+sys.path.append("Models/")
+from bibrank import get_weights
+
+from Models import *
+
+
+def mask(text1: str, text2: str) -> List[str]:
     """
     a simple vectorization function
     """
@@ -30,69 +34,73 @@ def mask(text1, text2):
             vectors[phrase] = base
             base += 1
         vector2.append(vectors[phrase])
-    return  vector1, vector2
+    return vector1, vector2
 
 
-def get_recall(l1,l2):
-    c = 0.0
-    C = float(len(l1))
+def get_recall(l1: List[str], l2: List[str]) -> float:
+    occur_count = 0.0
+
     for each in l2:
         if each in l1:
-            c += 1.0
-    return c/C
+            occur_count += 1.0
+    return occur_count / float(len(l1))
 
 
-def get_precision(l1,l2):
-    c = 0.0
-    C = float(len(l2))
+def get_precision(l1: List[str], l2: List[str]) -> float:
+    occur_count = 0.0
+
     for each in l2:
         if each in l1:
-            c += 1.0
-    return c/C
+            occur_count += 1.0
+    return occur_count / float(len(l2))
 
 
-def get_f1(r,p):
+def get_f1(r: float, p: float) -> float:
     if r + p == 0:
         return 0.0
-    return (2.0*p*r)/(p+r)
+    return (2.0 * p * r) / (p + r)
 
 
-def get_Rprecision (phrase1, phrase2):
+def get_Rprecision(phrase1: List[str], phrase2: List[str]) -> float:
     """
     relaxed since it uses "in" instead of checkign the position of words
     """
-    c = 0.0
+    occur_count = 0.0
 
     for w1 in phrase2.split():
         if w1 in phrase1:
-            c += 1
+            occur_count += 1
 
-    return c/max(len(phrase2.split()), len(phrase1.split()))
+    return occur_count / max(len(phrase2.split()), len(phrase1.split()))
 
 
-def get_all_Rprecision (gold_keywords, predicted_keywords):
-    scores=[]
+def get_all_Rprecision(
+    gold_keywords: List[str], predicted_keywords: List[str]
+) -> float:
+    scores = []
     for gphrase in gold_keywords:
-        rscores=[]
+        rscores = []
         for pphrase in predicted_keywords:
-            rscores.append(get_Rprecision(gphrase,pphrase))
+            rscores.append(get_Rprecision(gphrase, pphrase))
         scores.append(max(rscores))
 
     return sum(scores) / len(scores)
 
 
-def get_scores (gold_keywords, predicted_keywords):
+def get_scores(gold_keywords: List[str], predicted_keywords: List[str]) -> List[float]:
     recall = get_recall(gold_keywords, predicted_keywords)
     precision = get_precision(gold_keywords, predicted_keywords)
     f1 = get_f1(recall, precision)
     Rprecision = get_all_Rprecision(gold_keywords, predicted_keywords)
-
-
-
     return f1, recall, precision, Rprecision
 
 
-def get_all_scores (gold_keywords, predicted_keywords, text=None, adjust=False):
+def get_all_scores(
+    gold_keywords: List[str],
+    predicted_keywords: List[str],
+    text: str = "",
+    adjust: bool = False,
+) -> List:
     """
     SemEval-2010 Task 5, micro averaged f1, recall, precision
     """
@@ -113,13 +121,17 @@ def get_all_scores (gold_keywords, predicted_keywords, text=None, adjust=False):
     return metrics, adjusted_metrics
 
 
-def get_key_abs(filepath,year1=1900,year2=2020, bib_files=[], types=[], journals=[], count=None, rand=False):
-    """{'science-history-journals': 'Journals on the history, philosophy, and popularization of mathematics and
-       science', 'compsci': 'Computer science journals and topics', 'acm': 'ACM Transactions', 'cryptography':
-       'Cryptography', 'fonts': 'Fonts and typography', 'ieee': 'IEEE journals', 'computational': 'Computational/quantum
-       chemistry/physics journals', 'numerical': 'Numerical analysis journals', 'probstat': 'Probability and statistics
-       journals', 'siam': 'SIAM journals', 'math': 'Mathematics journals', 'mathbio': 'Mathematical and computational
-       biology'} """
+def get_key_abs(
+    filepath: str,
+    year1: int = 1900,
+    year2: int = 2020,
+    bib_files: List[str] = [],
+    types: List[str] = [],
+    journals: List[str] = [],
+    count: int = None,
+    rand: bool = False,
+):
+
     jfile = open("Datasets/bib_info.json").read()
     tables, names = json.loads(jfile)
     new_tables = {}
@@ -129,11 +141,6 @@ def get_key_abs(filepath,year1=1900,year2=2020, bib_files=[], types=[], journals
             new_tables[table].append(list(f.keys())[0])
 
     df = read_parquet(filepath)
-
-    #df =df[df["bib_file"]=="ibmjrd.bib"]
-    #print(df.columns)
-
-    #exit()
 
     if bib_files:
         try:
@@ -145,12 +152,10 @@ def get_key_abs(filepath,year1=1900,year2=2020, bib_files=[], types=[], journals
         types_list = []
         for type in types:
             types_list.extend(new_tables[type])
-        df = df[df['bib_file'].isin(types_list)]
+        df = df[df["bib_file"].isin(types_list)]
 
     if journals:
         df = df[df["journal"].isin(journals)]
-
-
 
     # year
     try:
@@ -167,10 +172,8 @@ def get_key_abs(filepath,year1=1900,year2=2020, bib_files=[], types=[], journals
 
     keywords = df["keywords"].tolist()
     abstracts = df["abstract"].tolist()
-    c1,c2=0.0,0.0
+    c1, c2 = 0.0, 0.0
 
-    print(df.shape)
-    print(df["year"].value_counts())
     processed_keywords = []
     for i in range(len(keywords)):
         if ";" in keywords[i]:
@@ -191,79 +194,137 @@ def get_key_abs(filepath,year1=1900,year2=2020, bib_files=[], types=[], journals
                 new.append(clean(key))
         processed_keywords.append(new)
 
-
     return processed_keywords, abstracts
 
 
-def eval_file(filepath, model, year1=1900, year2=2020, bib_files=[], types=[], journals=[], limit=None, rand=False, log=True,
-              model_param = "",outputpaths= ["output.json","output.tsv"], bib_weights = {}):
+def eval_file(
+    filepath,
+    model,
+    year1=1900,
+    year2=2020,
+    bib_files=[],
+    types=[],
+    journals=[],
+    limit=None,
+    rand=False,
+    log=True,
+    model_param="",
+    outputpaths=["output.json", "output.tsv"],
+    bib_weights={},
+    top_n=10,
+):
 
     t1 = time.time()
 
-
-    keywords_gold, abstracts = get_key_abs(filepath, year1, year2, bib_files, types, journals, limit, rand)
+    keywords_gold, abstracts = get_key_abs(
+        filepath, year1, year2, bib_files, types, journals, limit, rand
+    )
 
     if bib_weights:
-        keywords_gold_w, t = get_key_abs(filepath=bib_weights["dataset"], year1 = bib_weights["year1"], year2= bib_weights["year2"], types = bib_weights["types"])
+        keywords_gold_w, t = get_key_abs(
+            filepath=bib_weights["dataset"],
+            year1=bib_weights["year1"],
+            year2=bib_weights["year2"],
+            types=bib_weights["types"],
+        )
 
         weights = get_weights(keywords_gold_w)
 
         model = BibRank(weights)
 
-
-
-
-
     all_scores = []
     all_scores_adjust = []
     T0 = 0.0
     T1 = 0.0
-
+    data_file_ = open(model.model_name.lower() + ".tsv", "w")
     for i in tqdm(range(len(keywords_gold))):
 
         t3 = time.time()
 
-        predicted_keywords = model.get_keywords(abstracts[i])
+        predicted_keywords = model.get_keywords(abstracts[i], n=top_n)
 
         t4 = time.time()
         try:
-            scores = get_all_scores(keywords_gold[i], predicted_keywords[0], abstracts[i], adjust=True)
+            scores = get_all_scores(
+                keywords_gold[i], predicted_keywords[0], abstracts[i], adjust=True
+            )
 
         except:
-            scores = [[0.0, 0.0, 0.0,0.0] , [0.0, 0.0, 0.0,0.0]]
+            scores = [[0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0]]
+
+        data_line = [abstracts[i], keywords_gold[i], predicted_keywords[0]]
+        data_line.extend(scores)
+        data_line = [str(i) for i in data_line]
+        data_line = "\t".join(data_line)
+        data_file_.write(data_line)
+        data_file_.write("\n")
 
         t5 = time.time()
         all_scores.append(scores[0])
         if scores[1]:
             all_scores_adjust.append(scores[1])
-        T0 += (t4 - t3)
-        T1 += (t5 - t4)
+        T0 += t4 - t3
+        T1 += t5 - t4
     all_scores = pd.DataFrame(all_scores)
     all_scores_adjust = pd.DataFrame(all_scores_adjust)
-
 
     counts = [len(all_scores), len(all_scores_adjust)]
 
     all_scores = list(all_scores.mean(axis=0))
     all_scores_adjust = list(all_scores_adjust.mean(axis=0))
-    t = time.time() - t1
-    label = ''.join(random.choice('1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM')for x in range(8))
+    data_line = list(all_scores)
+    data_line.extend(all_scores_adjust)
+    data_line = [str(i) for i in data_line]
+    data_line = "\t".join(data_line)
+    data_file_.write(data_line)
+    data_file_.write("\n")
 
-    output = {"label": label, "file_path": filepath, "year1": year1, "year2": year2, "bib_files": bib_files,
-              "types": types, "journals": journals, "limit": limit,
-              "model_name": model.model_name, "model_param": model_param, "counts": counts,
-              "scores":all_scores, "scores_adjusted": all_scores_adjust,
-              "time": t, "random":rand }
+    t = time.time() - t1
+    label = "".join(
+        random.choice("1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM")
+        for x in range(8)
+    )
+
+    output = {
+        "label": label,
+        "file_path": filepath,
+        "year1": year1,
+        "year2": year2,
+        "bib_files": bib_files,
+        "types": types,
+        "journals": journals,
+        "limit": limit,
+        "model_name": model.model_name,
+        "model_param": model_param,
+        "counts": counts,
+        "scores": all_scores,
+        "scores_adjusted": all_scores_adjust,
+        "time": t,
+        "random": rand,
+    }
     if log:
         f1 = open(outputpaths[0], "a")
         json.dump(output, f1)
         f1.write("\n")
 
-        with open(outputpaths[1], 'a', newline='') as out_file:
-            tsv_writer = csv.writer(out_file, delimiter='\t')
-            tsv_writer.writerow([label, model.model_name, filepath, str(counts[0]), str(counts[1]),
-            str(all_scores[0]), str(all_scores[1]), str(all_scores[2]), str(all_scores[3]),
-            str(all_scores_adjust[0]), str(all_scores_adjust[1]), str(all_scores_adjust[2]), str(all_scores_adjust[3])])
+        with open(outputpaths[1], "a", newline="") as out_file:
+            tsv_writer = csv.writer(out_file, delimiter="\t")
+            tsv_writer.writerow(
+                [
+                    label,
+                    model.model_name,
+                    filepath,
+                    str(counts[0]),
+                    str(counts[1]),
+                    str(all_scores[0]),
+                    str(all_scores[1]),
+                    str(all_scores[2]),
+                    str(all_scores[3]),
+                    str(all_scores_adjust[0]),
+                    str(all_scores_adjust[1]),
+                    str(all_scores_adjust[2]),
+                    str(all_scores_adjust[3]),
+                ]
+            )
 
     return all_scores, all_scores_adjust
-
--- Models/Models.py
+++ Models/Models.py
@@ -3,7 +3,7 @@ import logging
 import sys
 import subprocess
 
-from position_rank import position_rank_mod
+from bibrank import bibrank
 from tokenizer import StanfordCoreNlpTokenizer
 
 import pke
@@ -13,19 +13,21 @@ from keybert import KeyBERT
 
 import spacy
 import en_core_web_sm
-from textacy.ke import sgrank
-from textacy.ke import scake
-from textacy.ke import textrank, yake
+from textacy.extract.keyterms import sgrank
+from textacy.extract.keyterms import scake
+from textacy.extract.keyterms import textrank, yake
 
-from bert_serving.client import BertClient
-path_uke = "unsupervised_keyword_extraction/" #path for the dir from  https://github.com/AnzorGozalishvili/unsupervised_keyword_extraction
+# from bert_serving.client import BertClient
+path_uke = "Models/unsupervised_keyword_extraction/"  # path for the dir from  https://github.com/AnzorGozalishvili/unsupervised_keyword_extraction
 sys.path.append(path_uke)
-from model.embedrank_transformers import EmbedRankTransformers #uncomment to use EmbedRank
+# from model.embedrank_transformers import EmbedRankTransformers #uncomment to use EmbedRank
 
 import numpy as np
 import pickle
 import nltk
 from nltk.corpus import stopwords
+
+from summa import keywords
 
 # import silence_tensorflow.auto
 # from tensorflow.python.keras.models import Model, Sequential, model_from_json, load_model
@@ -33,32 +35,29 @@ from nltk.corpus import stopwords
 # from tensorflow.python.keras.preprocessing.sequence import pad_sequences
 
 
-
 class KeyModel:
-    """ Main Model class
-    """
+    """Main Model class"""
+
     def __init__(self):
         pass
 
     def normalize_weights(self, weights):
         max_value = max(weights)
         min_val = min(weights)
-        normalized =[]
+        normalized = []
         for w in weights:
-            new_w = (w-min_val)/(max_value-min_val)
+            new_w = (w - min_val) / (max_value - min_val)
             normalized.append(new_w)
-        return  normalized
+        return normalized
 
 
+class PKE(KeyModel):
+    """Class for pke models developed by Florian Boudin"""
 
-class PKE (KeyModel):
-    """Class for pke models developed by Florian Boudin
-    """
-
-    def __init__(self, model_name, models=None, _df_counts=None, frequency_path = None):
+    def __init__(self, model_name, models=None, _df_counts=None, frequency_path=None):
         self.df = None
 
-        self.model_name = "PKE_"+model_name
+        self.model_name = "PKE_" + model_name
         if model_name.lower() == "topicrank":
             self.model = pke.unsupervised.TopicRank()
         elif model_name.lower() == "textrank":
@@ -85,11 +84,13 @@ class PKE (KeyModel):
         elif model_name.lower() == "wingnus":
             self.model = pke.supervised.WINGNUS()
         else:
-            raise Exception("{} is not a model name defined in PKE library".format(model_name))
+            raise Exception(
+                "{} is not a model name defined in PKE library".format(model_name)
+            )
         if models:
             self.model._models = models
         else:
-            self.model._models = os.path.join(os.path.dirname(pke.__file__), 'models')
+            self.model._models = os.path.join(os.path.dirname(pke.__file__), "models")
 
         if _df_counts:
             self.model._df_counts = _df_counts
@@ -97,8 +98,8 @@ class PKE (KeyModel):
         else:
             self._df_counts = os.path.join(self.model._models, "df-semeval2010.tsv")
 
-    def get_keywords(self, text, n=10, normalization='None'):
-        self.model.load_document(text, language='en', normalization=normalization)
+    def get_keywords(self, text, n=10, normalization="None"):
+        self.model.load_document(text, language="en", normalization=normalization)
 
         # keyphrase candidate selection, in the case of TopicRank: sequences of nouns
         # and adjectives (i.e. `(Noun|Adj)*`)
@@ -108,7 +109,7 @@ class PKE (KeyModel):
         if not self.df:
             self.model.candidate_weighting()
         else:
-            self.model.candidate_weighting(df = self.df)
+            self.model.candidate_weighting(df=self.df)
 
         # N-best selection, keyphrases contains the 10 highest scored candidates as
         # (keyphrase, score) tuples
@@ -119,22 +120,34 @@ class PKE (KeyModel):
             keyphrases, weights = None, None
         return keyphrases, weights
 
-    def create_doc_frequency(self, texts, outpath= "output.txt"):
-        path = os.path.join(os.path.dirname(pke.__file__), 'models/temp/')
+    def create_doc_frequency(self, texts, outpath="output.txt"):
+        path = os.path.join(os.path.dirname(pke.__file__), "models/temp/")
         for i in range(len(texts)):
-            fpath = os.path.join(path,"f"+str(i)+".txt")
-            open(fpath,"w",encoding="utf-8").write(texts[i])
+            fpath = os.path.join(path, "f" + str(i) + ".txt")
+            open(fpath, "w", encoding="utf-8").write(texts[i])
         opath = os.path.join(path, outpath)
-        compute_document_frequency(path, opath,"txt")
+        compute_document_frequency(path, opath, "txt")
 
 
-class keyBert (KeyModel):
+class keyBert(KeyModel):
     def __init__(self):
-        self.model = KeyBERT ('allenai/scibert_scivocab_uncased') #('distilbert-base-nli-mean-tokens')
+        self.model = KeyBERT(
+            "allenai/scibert_scivocab_uncased"
+        )  # ('distilbert-base-nli-mean-tokens')
         self.model_name = "keyBert"
+
     def get_keywords(self, text, n=10):
         try:
-            keyphrases, weights = list(map(list, zip(*self.model.extract_keywords(text,keyphrase_ngram_range=(1, 3), top_n=n))))
+            keyphrases, weights = list(
+                map(
+                    list,
+                    zip(
+                        *self.model.extract_keywords(
+                            text, keyphrase_ngram_range=(1, 3), top_n=n
+                        )
+                    ),
+                )
+            )
 
         except:
             logging.warning("model was not able to retrieve keywords")
@@ -143,19 +156,20 @@ class keyBert (KeyModel):
         return keyphrases, weights
 
 
-class Textacy (KeyModel):
+class Textacy(KeyModel):
     def __init__(self, model_name):
         self.nlp = en_core_web_sm.load()
-        self.model_name = "Textacy_"+model_name
+        self.model_name = "Textacy_" + model_name
 
     def get_keywords(self, text, n=10):
         try:
             doc = self.nlp(text)
+
         except:
             raise Exception("Couldn't load Spacy model")
 
         try:
-            if "sgrank" in self.model_name :
+            if "sgrank" in self.model_name:
                 keyphrases, weights = list(map(list, zip(*sgrank(doc, topn=n))))
             elif "scake" in self.model_name:
                 keyphrases, weights = list(map(list, zip(*scake(doc, topn=n))))
@@ -164,14 +178,18 @@ class Textacy (KeyModel):
             elif "yake" in self.model_name:
                 keyphrases, weights = list(map(list, zip(*yake(doc, topn=n))))
             else:
-                raise Exception("{} is not a model name defined in textacy library".format(self.model_name))
+                raise Exception(
+                    "{} is not a model name defined in textacy library".format(
+                        self.model_name
+                    )
+                )
         except:
             logging.warning("model was not able to retrieve keywords")
             keyphrases, weights = None, None
         return keyphrases, weights
 
 
-class BertEmbedRank (KeyModel):
+class BertEmbedRank(KeyModel):
     def __init__(self, pre_load=True, n=10, bert_path=None):
         """
 
@@ -183,50 +201,53 @@ class BertEmbedRank (KeyModel):
         self.model_name = "BertEmbedRank"
         if not bert_path:
             bert_path = "unsupervised_keyword_extraction/cased_L-12_H-768_A-12/"
-        #try:
-        #with open("stdout.txt", "wb") as out, open("stderr.txt", "wb") as err:
+        # try:
+        # with open("stdout.txt", "wb") as out, open("stderr.txt", "wb") as err:
         #    self.p = subprocess.Popen(["bert-serving-start", "-model_dir", bert_path, "-num_worker=4"], stdout=out, stderr=err)
-        self.bc = BertClient(output_fmt='list', timeout=120000)
-        #except:
+        self.bc = BertClient(output_fmt="list", timeout=120000)
+        # except:
         #    logging.warning("BertClient is not working")
 
-        self.nlp = spacy.load("en_core_web_lg", disable=['ner'])
+        self.nlp = spacy.load("en_core_web_lg", disable=["ner"])
         if pre_load:
-            self.model = EmbedRankTransformers(nlp=self.nlp,
-                                          dnn=self.bc,
-                                          perturbation='replacement',
-                                          emb_method='subtraction',
-                                          mmr_beta=0.55,
-                                          top_n=n,
-                                          alias_threshold=0.8)
+            self.model = EmbedRankTransformers(
+                nlp=self.nlp,
+                dnn=self.bc,
+                perturbation="replacement",
+                emb_method="subtraction",
+                mmr_beta=0.55,
+                top_n=n,
+                alias_threshold=0.8,
+            )
         else:
-            self.model=None
+            self.model = None
 
     def get_keywords(self, text, n=10):
         if not self.model:
-            self.model = EmbedRankTransformers(nlp=self.nlp,
-                                   dnn=self.bc,
-                                   perturbation='replacement',
-                                   emb_method='subtraction',
-                                   mmr_beta=0.55,
-                                   top_n=n,
-                                   alias_threshold=0.8)
+            self.model = EmbedRankTransformers(
+                nlp=self.nlp,
+                dnn=self.bc,
+                perturbation="replacement",
+                emb_method="subtraction",
+                mmr_beta=0.55,
+                top_n=n,
+                alias_threshold=0.8,
+            )
 
-        #marked_target, keywords, keyword_relevance = self.model.fit(text)
+        # marked_target, keywords, keyword_relevance = self.model.fit(text)
 
         keyphrases, weights = list(map(list, zip(*self.model.extract_keywords(text))))
 
-        return  keyphrases, weights
+        return keyphrases, weights
 
     def exit(self):
         self.p.terminate()
 
 
-
-class BiLSTM (KeyModel):
+class BiLSTM(KeyModel):
     def __init__(self, json_config_f, model_f, tokenizer_f):
-        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-        self.stop_words = set(stopwords.words('english'))
+        os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+        self.stop_words = set(stopwords.words("english"))
         with open(json_config_f) as json_file:
             json_config = json_file.read()
         self.model = model_from_json(json_config)
@@ -235,7 +256,7 @@ class BiLSTM (KeyModel):
         self.model.load_weights(model_f)
 
         tokenizer = Tokenizer()
-        with open(tokenizer_f, 'rb') as handle:
+        with open(tokenizer_f, "rb") as handle:
             self.tokenizer = pickle.load(handle)
 
     def get_keywords(self, text, n=10):
@@ -248,7 +269,9 @@ class BiLSTM (KeyModel):
         actual_tokens = [inv_map_tokens[i] for i in actual_tokens[0]]
         tokens = actual_tokens
         input_ = self.tokenizer.texts_to_sequences([text])
-        input_ = pad_sequences(input_, padding="post", truncating="post", maxlen=25, value=0)
+        input_ = pad_sequences(
+            input_, padding="post", truncating="post", maxlen=25, value=0
+        )
         output = self.model.predict([input_])
         output = np.argmax(output, axis=-1)
         where_ = np.where(output[0] == 1)[0]
@@ -256,44 +279,53 @@ class BiLSTM (KeyModel):
         output_keywords = [i for i in output_keywords if i not in self.stop_words]
         output_keywords = list(set(output_keywords))
         k = output_keywords[:n]
-        return k, [0.0]*len(k)
-
+        return k, [0.0] * len(k)
 
 
 class BibRank(KeyModel):
     """
     java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9000 -timeout 15000
     """
-    def __init__(self, weights=None, wiki=False):
-        self.tokenizer = StanfordCoreNlpTokenizer("http://localhost", port = 9000)
+
+    def __init__(self, weights=None, wiki=False, pos=True):
+        self.tokenizer = StanfordCoreNlpTokenizer("http://localhost", port=9000)
         self.weights = weights
         self.wiki = wiki
-        self.model_name = "PositionRankMod"
+        self.model_name = "bibrank"
+
     def get_keywords(self, text, n=10):
-        keyphrases_p = position_rank_mod(text.split(), self.tokenizer, alpha=0.85, window_size=2, num_keyphrase=n, weights=self.weights,
-                                     wiki=self.wiki)
+        keyphrases_p = bibrank(
+            text.split(),
+            self.tokenizer,
+            alpha=0.85,
+            window_size=2,
+            num_keyphrase=n,
+            weights=self.weights,
+            wiki=self.wiki,
+        )
         return list(map(list, zip(*keyphrases_p)))
 
-class CollabRank (KeyModel):
+
+class CollabRank(KeyModel):
     def __init__(self, collab_documents):
         self.collab_documents = collab_documents
         self.model_name = "CollabRank"
 
     def get_keywords(self, text, n=10):
-        pos = {'NOUN', 'PROPN', 'ADJ'}
+        pos = {"NOUN", "PROPN", "ADJ"}
         extractor = pke.unsupervised.CollabRank()
 
-        extractor.load_document(text,
-                                language='en',
-                                normalization=None)
+        extractor.load_document(text, language="en", normalization=None)
         extractor.candidate_selection(pos=pos)
-        extractor.candidate_weighting(collab_documents=self.collab_documents, window=10,
-                                      pos=pos)
+        extractor.candidate_weighting(
+            collab_documents=self.collab_documents, window=10, pos=pos
+        )
 
         keyphrases, weights = list(map(list, zip(*extractor.get_n_best(n=n))))
         return keyphrases, weights
 
-text = '''
+
+text = """
    Over the past few years, there has been a strong and growing interest
    in faster network technologies such as FDDI and ATM. However, the
    perceived throughput at the application level has not always increased
@@ -321,12 +353,11 @@ text = '''
    a range of video frame sizes: CIF(25K), QCIF(101K), and SCIF(405K).
 
 
-'''
+"""
 
-'''
+"""
 extractor = Textacy ("yake")
 k,w = extractor.get_keywords(text)
 print(k)
 print(w)
-'''
-
+"""
